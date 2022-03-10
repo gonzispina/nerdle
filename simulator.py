efdeb603cebb873ec69bd36ed30d40cd5c9a6630
Gon <gspina1995@gmail.com>
2022-03-10 16:22:08 -0300

    Finished with the lower bound scores strategy

--- simulator.py
+++ simulator.py
@@ -1,4 +1,5 @@
 from tqdm import tqdm as ProgressDisplay
+from scipy.stats import entropy
 import itertools as it
 import random
 
@@ -118,6 +119,102 @@ def get_possible_words(guess, pattern, possibilities):
     return list(np.array(possibilities)[all_patterns == pattern])
 
 
+def get_pattern_distributions(allowed_words, possible_words, weights):
+    """
+    For each possible guess in allowed_words, this finds the probability
+    distribution across all of the 3^8 nerdle patterns you could see, assuming
+    the possible answers are in possible_words with associated probabilities
+    in weights.
+    It considers the pattern hash grid between the two lists of words, and uses
+    that to bucket together words from possible_words which would produce
+    the same pattern, adding together their corresponding probabilities.
+    For example if A = words[a] and B = words[b] form the same pattern with C=words[c]
+    then when C is the answer and A, B the possibilities, then the probabilities will be
+    lower in the distribution since both puts the same amount of information for the
+    given answer.
+    """
+    pattern_matrix = get_pattern_matrix(allowed_words, possible_words)
+
+    n = len(allowed_words)
+    distributions = np.zeros((n, 3**8))
+    n_range = np.arange(n)
+    for j, prob in enumerate(weights):
+        distributions[n_range, pattern_matrix[:, j]] += prob
+    return distributions
+
+
+def get_bucket_sizes(allowed_words, possible_words):
+    """
+    Returns a (len(allowed_words), 243) shape array reprenting the size of
+    word buckets associated with each guess in allowed_words
+    """
+    weights = np.ones(len(possible_words))
+    return get_pattern_distributions(allowed_words, possible_words, weights)
+
+
+def get_bucket_counts(allowed_words, possible_words):
+    """
+    Returns the number of separate buckets that each guess in allowed_words
+    would separate possible_words into
+    """
+    bucket_sizes = get_bucket_sizes(allowed_words, possible_words)
+    return (bucket_sizes > 0).sum(1)
+
+
+def get_score_lower_bounds(allowed_words, possible_words):
+    """
+    Assuming a uniform distribution on how likely each element
+    of possible_words is, this gives a lower bound on the
+    possible score for each word in allowed_words
+    """
+    bucket_counts = get_bucket_counts(allowed_words, possible_words)
+    N = len(possible_words)
+    # Probabilities of getting it in 1
+    p1s = np.array([w in possible_words for w in allowed_words]) / N
+    # Probabilities of getting it in 2
+    p2s = bucket_counts / N - p1s
+    # Otherwise, assume it's gotten in 3 (which is optimistics)
+    p3s = 1 - bucket_counts / N
+    return p1s + 2 * p2s + 3 * p3s
+
+
+def get_entropies(allowed_words, possible_words):
+    weights = np.ones(len(possible_words))
+    distributions = get_pattern_distributions(allowed_words, possible_words, weights)
+    return entropy(distributions, base=2, axis=0)
+
+
+def optimal_guess(allowed_words, possible_words,
+                  priors=[],
+                  look_two_ahead=False,
+                  optimize_for_uniform_distribution=False,
+                  purely_maximize_information=False,
+                  ):
+
+    """
+    if purely_maximize_information:
+        if len(possible_words) == 1:
+            return possible_words[0]
+        weights = get_weights(possible_words, priors)
+        ents = get_entropies(allowed_words, possible_words, weights)
+        return allowed_words[np.argmax(ents)]
+
+    """
+
+    # Just experimenting here...
+    # if optimize_for_uniform_distribution:
+    expected_scores = get_score_lower_bounds(
+        allowed_words, possible_words
+    )
+    # else:
+    #    expected_scores = get_expected_scores(
+    #        allowed_words, possible_words, priors,
+    #        look_two_ahead=look_two_ahead
+    #    )
+
+    return allowed_words[np.argmin(expected_scores)]
+
+
 def simulate():
     words = get_all_words()
 
@@ -139,7 +236,7 @@ def simulate():
             patterns.append(pattern)
 
             possibilities = get_possible_words(guess, pattern, possibilities)
-            guess = possibilities[random.randint(0, len(possibilities)-1)]
+            guess = optimal_guess(words, possibilities)
             score += 1
 
         scores = np.append(scores, [score])
@@ -159,4 +256,4 @@ def simulate():
 
 
 if __name__ == "__main__":
-    simulate()
+    print(simulate())
